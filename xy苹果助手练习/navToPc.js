5c97bd2a0fedc86d4da224594fca9a5143c4dc7a
zzwzzw123 <42956081+zzwzzw123@users.noreply.github.com>
2018-09-08 16:35:17 +0800

    新增移动端web
    新增移动端web

---
+++ xy苹果助手练习/navToPc.js
@@ -0,0 +1,10 @@
+//判断如果是pc端浏览器自动跳转到pc/index。html
+//如果是苹果安卓手机自动跳转到moblie/index.html
+var userAgent = navigator.userAgent.toLowerCase();
+
+if (userAgent.indexOf('android') == -1 
+	&& userAgent.indexOf('iphone') == -1
+	&& userAgent.indexOf('ipad') == -1) {
+		
+	location.href = '../pc/index.html';
+}
